1c6f0e209cae0d0174d65fd338cb08da448ab1fc
barryz <barryzxb@gmail.com>
2017-11-11 17:42:12 +0800

    first commit

---
+++ src/squeeze.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void squeeze(char s[], int c);
+
+int main(void)
+{
+    const int SIZE = 11;
+    char f = 'L';
+    char FFFF[SIZE] = {'H',
+                       'E',
+                       'L',
+                       'L',
+                       'O',
+                       'W',
+                       'O',
+                       'R',
+                       'L',
+                       'D',
+                       '\0'};
+    printf("Before squeeze: %s\n", FFFF);
+    squeeze(FFFF, f);
+    printf("After squeeze: %4s\n", FFFF);
+    return 0;
+}
+
+void squeeze(char s[], int c)
+{
+    int i, j;
+    for (i = j = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] != c)
+        {
+            s[j++] = s[i];
+            /*
+            equals:
+            s[j] = s[i]; j++;
+            */
+        }
+    }
+    s[j] = '\0';
+    return;
+}
